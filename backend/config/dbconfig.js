ab7a4e3e93d8f67a95568cfec77f298ed4262590
since33337 <armelsince@gmail.com>
2025-07-06 02:22:18 +0200

    connexion a freeDB via .env

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -1,18 +1,21 @@
-const mysql= require('mysql');
+import mysql from 'mysql';
+import dotenv from 'dotenv';
 
-// creer la connexion a la base de donnees 
-const db = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'',
-  database:'ecom'  
-})
-// connexion a la base de donnees 
-db.connect(err=>{
-    if(err){
-        console.error('Erreur de connexion a al base de donnees :',err);
-    }else{
-        console.log('connecte a la base de donnees mysql');
-    }
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-module.exports=db
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+    return;
+  }
+  console.log('Connecté à la base de données MySQL');
+});
+
+export default connection;
