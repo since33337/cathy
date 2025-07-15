e2a31f38924642e4c510136874edfa0a18010f8d
Armel Since <armelsince@gmail.com>
2025-07-15 23:12:09 +0200

    modification du fichier

--- essai/src/pages/addproduct.jsx
+++ essai/src/pages/addproduct.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './addProduct.css';
+import './addproduct.css';
 import { useNavigate } from 'react-router-dom';
 
 const AddProduct = () => {
